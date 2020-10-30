4cebadab9fcc6dd33bb833f513436c7eb08e37e6
elbeicktalat <talatelbeick@gmail.com>
2020-10-30 16:55:53 +0100

    add images_url

---
+++ lib/res/shared/images_url.dart
@@ -0,0 +1,7 @@
+String googleIcon = 'packages/auth_buttons/images/google_logo.png';
+String facebookIcon = 'packages/auth_buttons/images/facebook_logo_white.png';
+String appleBlackIcon = 'packages/auth_buttons/images/apple_logo_black.png';
+String appleWhiteIcon = 'packages/auth_buttons/images/apple_logo_white.png';
+String twitterIcon = 'packages/auth_buttons/images/twitter_logo.png';
+String twitterWhiteIcon = 'packages/auth_buttons/images/twitter_logo_white.png';
+String githubIcon = 'packages/auth_buttons/images/github_logo.png';
