35ad42f7348e9358ea5d6119b86ae6ae2bfb640e
elbeicktalat <talatelbeick@gmail.com>
2021-02-13 12:48:22 +0100

    move auth_button to shared dir

--- lib/res/buttons/custom_auth_button.dart
+++ lib/res/buttons/custom_auth_button.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:auth_buttons/res/buttons/auth_button.dart';
+import 'package:auth_buttons/res/shared/auth_button.dart';
 import 'package:auth_buttons/res/shared/auth_style.dart';
 import 'package:flutter/material.dart';
 
--- lib/res/shared/auth_button.dart
+++ lib/res/shared/auth_button.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:auth_buttons/res/shared/auth_icons.dart';
 import 'package:auth_buttons/res/shared/auth_style.dart';
 import 'package:auth_buttons/res/shared/button_contents.dart';
 import 'package:auth_buttons/res/shared/shared_button.dart';
