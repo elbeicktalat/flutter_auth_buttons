bb0964263389bb6c55583023647fba49c56ad753
Talat El Beick <talatelbeick@gmail.com>
2021-04-14 20:37:02 +0200

    docs the style api (#18)

--- lib/src/shared/auth_button_style_button.dart
+++ lib/src/shared/auth_button_style_button.dart
@@ -16,12 +16,44 @@ abstract class AuthButtonStyleButton extends StatefulWidget {
     required this.rtl,
   }) : super(key: key);
 
+  ///**[onPressed]** is a void function well be called when the button is pressed.
   final VoidCallback? onPressed;
+
+  ///**[onLongPress]** is a void function well be called when the button is pressed for long.
   final VoidCallback? onLongPress;
+
+  ///**[style]** define the entire button style, like buttonColor, iconSize,
+  /// width, and other.
   final AuthButtonStyle? style;
+
+  ///**[text]** Define text in the button.
+  ///
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/assets/style-default.png)
+  ///
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/assets/text.png)
   final String? text;
+
+  ///**[darkMode]** Define if the theme of the button is dark or light,
+  ///the default value is [false].
+  ///
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/assets/style-default.png)
+  ///
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/assets/dark-mode-default.png)
+  ///
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/assets/style-icon.png)
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/assets/dark-mode-icon.png)
+
   final bool darkMode;
+
+  ///**[isLoading]** A boolean variable which define if something is loading,
+  /// if so will be show a progress indicator.
   final bool isLoading;
+
+  ///**[rtl]** A boolean variable needed to support the **right to left** languages.
+  ///
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/buttons/google.png)
+  ///
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/assets/rtl.png)
   final bool rtl;
 
   @override
