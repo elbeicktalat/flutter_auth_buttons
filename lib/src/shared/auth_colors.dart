50e841ba9c08ac719eb7d6af2f33039d45a9f816
elbeicktalat <talatelbeick@gmail.com>
2021-04-18 11:51:23 +0200

    update facebook button color

--- lib/src/shared/auth_colors.dart
+++ lib/src/shared/auth_colors.dart
@@ -6,8 +6,7 @@ import 'package:flutter/material.dart';
 
 class AuthColors {
   static const Color darkMode = Color(0xff424242);
-  static const Color facebook = Color(0xff3a559f);
-  static const Color facebookSecondary = Color(0xff0676e8);
+  static const Color facebook = Color(0xff1a77f2);
   static const Color github = Color(0xff24292e);
   static const Color huawei = Color(0xffce0e2d);
 }
