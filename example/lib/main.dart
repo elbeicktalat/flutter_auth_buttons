80ec76aacdad0983891ddc5204bbff89d2c921c7
elbeicktalat <talatelbeick@gmail.com>
2021-05-02 13:59:55 +0200

    make example scrollable

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,105 +17,110 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     const String appName = 'Auth Buttons Example';
     const bool darkMode = false;
-    const AuthButtonType buttonType = AuthButtonType.secondary;
-    const AuthIconType authIconType = AuthIconType.outlined;
+    const AuthButtonType? buttonType = AuthButtonType.secondary;
+    const AuthIconType? authIconType = AuthIconType.outlined;
     return MaterialApp(
       title: appName,
       home: Scaffold(
         appBar: AppBar(
           title: const Text(appName),
         ),
-        body: Container(
-          width: double.infinity,
-          color: darkMode ? const Color(0xff303030) : Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              GoogleAuthButton(
-                onPressed: () {
-                  // your implementation
-                  setState(() {
-                    isLoading = !isLoading;
-                  });
-                },
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            color: darkMode ? const Color(0xff303030) : Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GoogleAuthButton(
+                    onPressed: () {
+                      // your implementation
+                      setState(() {
+                        isLoading = !isLoading;
+                      });
+                    },
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                  const Divider(),
+                  AppleAuthButton(
+                    onPressed: () {},
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                  const Divider(),
+                  FacebookAuthButton(
+                    onPressed: () {},
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                  const Divider(),
+                  GithubAuthButton(
+                    onPressed: () {},
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                  const Divider(),
+                  MicrosoftAuthButton(
+                    onPressed: () {},
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                  const Divider(),
+                  TwitterAuthButton(
+                    onPressed: () {},
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                  const Divider(),
+                  EmailAuthButton(
+                    onPressed: () {},
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                  const Divider(),
+                  HuaweiAuthButton(
+                    onPressed: () {},
+                    darkMode: darkMode,
+                    isLoading: isLoading,
+                    style: const AuthButtonStyle(
+                      buttonType: buttonType,
+                      iconType: authIconType,
+                    ),
+                  ),
+                ],
               ),
-              const Divider(),
-              AppleAuthButton(
-                onPressed: () {},
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
-              ),
-              const Divider(),
-              FacebookAuthButton(
-                onPressed: () {},
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
-              ),
-              const Divider(),
-              GithubAuthButton(
-                onPressed: () {},
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
-              ),
-              const Divider(),
-              MicrosoftAuthButton(
-                onPressed: () {},
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
-              ),
-              const Divider(),
-              TwitterAuthButton(
-                onPressed: () {},
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
-              ),
-              const Divider(),
-              EmailAuthButton(
-                onPressed: () {},
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
-              ),
-              const Divider(),
-              HuaweiAuthButton(
-                onPressed: () {},
-                darkMode: darkMode,
-                isLoading: isLoading,
-                style: const AuthButtonStyle(
-                  buttonType: buttonType,
-                  iconType: authIconType,
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
