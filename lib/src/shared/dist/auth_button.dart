88b7f62f5d912ed57087a4ff4f13c09f448e4be0
elbeicktalat <talatelbeick@gmail.com>
2021-11-02 15:20:01 +0100

    move into dist and remove all implementation with inheriting BaseAuthButton.

---
+++ lib/src/shared/dist/auth_button.dart
@@ -0,0 +1,31 @@
+// Copyright 2021 Talat El Beick. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:auth_buttons/src/shared/base/contracts/base_auth_button.dart';
+import 'package:auth_buttons/src/shared/dist/auth_button_style.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class AuthButton extends BaseAuthButton {
+  const AuthButton({
+    Key? key,
+    required VoidCallback? onPressed,
+    VoidCallback? onLongPress,
+    AuthButtonStyle? style,
+    String? text,
+    bool darkMode = false,
+    bool isLoading = false,
+    bool rtl = false,
+    TextStyle? textStyle,
+  }) : super(
+          key: key,
+          onPressed: onPressed,
+          onLongPress: onLongPress,
+          style: style ?? const AuthButtonStyle(),
+          text: text,
+          textStyle: textStyle,
+          darkMode: darkMode,
+          isLoading: isLoading,
+          rtl: rtl,
+        );
+}
