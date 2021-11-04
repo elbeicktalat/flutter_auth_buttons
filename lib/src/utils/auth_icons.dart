07f709b1568c27f1d38deff99143cef351d7d5b4
elbeicktalat <talatelbeick@gmail.com>
2021-11-04 16:38:50 +0100

    update images url

--- lib/src/utils/auth_icons.dart
+++ lib/src/utils/auth_icons.dart
@@ -4,65 +4,70 @@
 
 class AuthIcons {
   static const List<String> google = <String>[
-    'packages/auth_buttons/images/default/google.png',
-    'packages/auth_buttons/images/outlined/google.png',
-    'packages/auth_buttons/images/secondary/google.png',
+    'packages/auth_buttons/images/default/google.svg',
+    'packages/auth_buttons/images/outlined/google.svg',
+    'packages/auth_buttons/images/secondary/google.svg',
   ];
   static const List<String> facebook = <String>[
-    'packages/auth_buttons/images/default/facebook.png',
-    'packages/auth_buttons/images/outlined/facebook.png',
-    'packages/auth_buttons/images/secondary/facebook.png',
+    'packages/auth_buttons/images/default/facebook.svg',
+    'packages/auth_buttons/images/outlined/facebook.svg',
+    'packages/auth_buttons/images/secondary/facebook.svg',
   ];
   static const List<String> facebookWhite = <String>[
-    'packages/auth_buttons/images/default/facebook_white.png',
-    'packages/auth_buttons/images/outlined/facebook_white.png',
+    'packages/auth_buttons/images/default/facebook_white.svg',
+    'packages/auth_buttons/images/outlined/facebook_white.svg',
   ];
 
   static const List<String> apple = <String>[
-    'packages/auth_buttons/images/default/apple.png',
-    'packages/auth_buttons/images/outlined/apple.png',
-    'packages/auth_buttons/images/secondary/apple.png',
+    'packages/auth_buttons/images/default/apple.svg',
+    'packages/auth_buttons/images/outlined/apple.svg',
+    'packages/auth_buttons/images/secondary/apple.svg',
   ];
   static const List<String> appleWhite = <String>[
-    'packages/auth_buttons/images/default/apple_white.png',
-    'packages/auth_buttons/images/outlined/apple_white.png',
+    'packages/auth_buttons/images/default/apple_white.svg',
+    'packages/auth_buttons/images/outlined/apple_white.svg',
   ];
   static const List<String> twitter = <String>[
-    'packages/auth_buttons/images/default/twitter.png',
-    'packages/auth_buttons/images/outlined/twitter.png',
+    'packages/auth_buttons/images/default/twitter.svg',
+    'packages/auth_buttons/images/outlined/twitter.svg',
   ];
   static const List<String> twitterWhite = <String>[
-    'packages/auth_buttons/images/default/twitter_white.png',
-    'packages/auth_buttons/images/outlined/twitter_white.png',
+    'packages/auth_buttons/images/default/twitter_white.svg',
+    'packages/auth_buttons/images/outlined/twitter_white.svg',
   ];
-  static const String github =
-      'packages/auth_buttons/images/default/github_black.png';
-  static const String githubWhite =
-      'packages/auth_buttons/images/default/github_white.png';
+  static const List<String> github = [
+    'packages/auth_buttons/images/default/github_black.svg',
+    'packages/auth_buttons/images/outlined/github.svg',
+    'packages/auth_buttons/images/secondary/github.svg',
+  ];
+  static const List<String> githubWhite = [
+    'packages/auth_buttons/images/default/github_white.svg',
+    'packages/auth_buttons/images/outlined/github_white.svg',
+  ];
   static const List<String> microsoft = <String>[
-    'packages/auth_buttons/images/default/microsoft.png',
-    'packages/auth_buttons/images/outlined/microsoft.png',
-    'packages/auth_buttons/images/secondary/microsoft.png',
+    'packages/auth_buttons/images/default/microsoft.svg',
+    'packages/auth_buttons/images/outlined/microsoft.svg',
+    'packages/auth_buttons/images/secondary/microsoft.svg',
   ];
 
   static const List<String> email = <String>[
-    'packages/auth_buttons/images/default/email.png',
-    'packages/auth_buttons/images/outlined/email.png',
-    'packages/auth_buttons/images/secondary/email.png',
+    'packages/auth_buttons/images/default/email.svg',
+    'packages/auth_buttons/images/outlined/email.svg',
+    'packages/auth_buttons/images/secondary/email.svg',
   ];
   static const List<String> emailWhite = <String>[
-    'packages/auth_buttons/images/default/email_white.png',
-    'packages/auth_buttons/images/outlined/email_white.png',
-    'packages/auth_buttons/images/secondary/email_white.png',
+    'packages/auth_buttons/images/default/email_white.svg',
+    'packages/auth_buttons/images/outlined/email_white.svg',
+    'packages/auth_buttons/images/secondary/email_white.svg',
   ];
 
   static const List<String> huawei = <String>[
-    'packages/auth_buttons/images/default/huawei.png',
-    'packages/auth_buttons/images/outlined/huawei.png',
-    'packages/auth_buttons/images/secondary/huawei.png',
+    'packages/auth_buttons/images/default/huawei.svg',
+    'packages/auth_buttons/images/outlined/huawei.svg',
+    'packages/auth_buttons/images/secondary/huawei.svg',
   ];
   static const List<String> huaweiWhite = <String>[
-    'packages/auth_buttons/images/default/huawei_white.png',
-    'packages/auth_buttons/images/outlined/huawei_white.png',
+    'packages/auth_buttons/images/default/huawei_white.svg',
+    'packages/auth_buttons/images/outlined/huawei_white.svg',
   ];
 }
