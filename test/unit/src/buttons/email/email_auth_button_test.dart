14fdd71d495c5bcc789da82ab8534c8067daa48b
elbeicktalat <talatelbeick@gmail.com>
2021-11-15 16:56:50 +0100

    move previous email test into email package

--- test/unit/src/buttons/email/email_auth_button_test.dart
+++ test/unit/src/buttons/email/email_auth_button_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 Talat El Beick. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:auth_buttons/auth_buttons.dart';
 import 'package:flutter_test/flutter_test.dart';
 
