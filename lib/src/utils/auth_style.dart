3642d2a41a31b3b40035ea34ba9ab37603518ccf
elbeicktalat <talatelbeick@gmail.com>
2021-12-16 08:37:20 +0100

    docs AuthButtonType

--- lib/src/utils/auth_style.dart
+++ lib/src/utils/auth_style.dart
@@ -2,9 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+///[AuthButtonType] This enum make you able to choose the type of button.
 enum AuthButtonType {
-  ///[icon] Show only social media icon with around soft border.
+  ///[icon] Show only social media icon.
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/buttons/google-icon.png)
   icon,
+
+  ///[secondary] Very similar to default auth button, but this has tow colors,
+  ///the button color and the icon background color.
+  ///![](https://raw.githubusercontent.com/elbeicktalat/flutter_auth_buttons/master/doc/api/buttons/google-secondary.png)
   secondary,
 }
 
